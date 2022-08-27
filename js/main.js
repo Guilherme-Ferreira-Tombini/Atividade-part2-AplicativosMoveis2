cabb2d3507fe80e34e4697271c4b40852d8f6ead
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-08-27 10:35:58 -0300

    Adicionado js para adicionar e limpar o nome

--- js/main.js
+++ js/main.js
@@ -4,3 +4,37 @@ window.onload = () => {
         navigator.serviceWorker.register("./sw.js");
     }
 };
+
+function nome1(){
+    let nome = document.getElementById("equipe1").value;
+
+    if (nome != "") {
+        document.getElementById("texto").innerHTML = "Equipe: " + nome
+    } else{
+        document.getElementById("texto").innerHTML = "Porfavor, insira o nome da equipe!!"
+    }
+}
+
+function nome2(){
+    let nome = document.getElementById("equipe2").value;
+
+    if (nome != "") {
+        document.getElementById("texto2").innerHTML = "Equipe: " + nome
+    } else{
+        document.getElementById("texto2").innerHTML = "Porfavor, insira o nome da equipe!!"
+    }
+}
+
+function limpar(){
+    let nome = document.getElementById("equipe1");
+    let texto =  document.getElementById("texto");
+    nome.value = "";
+    texto.innerHTML = "";
+}
+
+function limpar2(){
+    let nome = document.getElementById("equipe2");
+    let texto =  document.getElementById("texto2");
+    nome.value = "";
+    texto.innerHTML = "";
+}
